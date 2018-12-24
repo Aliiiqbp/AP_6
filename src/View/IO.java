14be5ee75a08bf6c8f62cf4eebab7e9c1a44e4b0
ali <alialaee98@gmail.com>
2018-12-24 19:02:55 +0330

    Split command in IO completed

--- src/View/IO.java
+++ src/View/IO.java
@@ -1,8 +1,13 @@
 package src.View;
 
+import java.util.Scanner;
+
 public class IO {
 
-    public String[] getCommandStrings() {
-        return null;
+    public String[] splitCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String[] strings = string.split(" ");
+        return strings;
     }
 }
