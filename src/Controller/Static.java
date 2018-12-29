fb19ba16bba1773f6d97e3599038faa3c88699ed
ali <alialaee98@gmail.com>
2018-12-29 17:13:23 +0330

    Static class upgraded;
    - Pet animal statics created

--- src/Controller/Static.java
+++ src/Controller/Static.java
@@ -5,4 +5,46 @@ public class Static {
     public static final int firstLevel = 0;
     public static final double INITIAL_MONEY = 50.0;
     public static final int INITIAL_WELL_CAPACITY = 10;
+
+    // ANIMAL STATICS
+
+    //cat
+    public static final int CAT_SLEEP_TIME = 10;
+    public static final int CAT_SPEED = 38;
+    public static final int CAT_PRICE = 2500;
+
+    //dog
+    public static final int DOG_SPEED = 65;
+
+    //sheep
+    public static final int SHEEP_SPEED = 55;
+    public static final int SHEEP_HUNGRY_MOVING_SPEED = 110;
+    public static final int SHEEP_PRODUCTION_PERIOD = 20;
+    public static final int SHEEP_BITE_TIME = 10;
+    public static final int SHEEP_WAIT_TIME = 2;
+    public static final int SHEEP_HUNGRY_SPEED = 9;
+    public static final int SHEEP_HUMGRY_VALUE = 150;
+    public static final int SHEEP_BUY_COST = 1000;
+
+    //hen
+    public static final int HEN_SPEED = 50;
+    public static final int HEN_HUNGRY_MOVING_SPEED = 110;
+    public static final int HEN_PRODUCTION_PERIOD = 20;
+    public static final int HEN_BITE_TIME = 10;
+    public static final int HEN_WAIT_TIME = 2;
+    public static final int HEN_HUNGRY_SPEED = 3;
+    public static final int HEN_HUMGRY_VALUE = 50;
+    public static final int HEN_BUY_COST = 100;
+
+    //cow
+    public static final int COW_SPEED = 40;
+    public static final int COW_HUNGRY_MOVING_SPEED = 80;
+    public static final int COW_PRODUCTION_PERIOD = 20;
+    public static final int COW_BITE_TIME = 10;
+    public static final int COW_WAIT_TIME = 2;
+    public static final int COW_HUNGRY_SPEED = 18;
+    public static final int COW_HUMGRY_VALUE = 300;
+    public static final int COW_BUY_COST = 10000;
+
+
 }
