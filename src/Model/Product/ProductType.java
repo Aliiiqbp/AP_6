252e052e3961adc28d1e8238217942abb2cbc895
ali <aliiiqbp@gmail.com>
2018-12-31 17:32:23 +0330

    WorkShop created getProduct added to ProductType

--- src/Model/Product/ProductType.java
+++ src/Model/Product/ProductType.java
@@ -2,4 +2,17 @@ package src.Model.Product;
 
 public enum ProductType {
     EGG, FLOUR, GRASS, MILK, WOOL, WATER;
+
+    public Product getProduct(ProductType productType, double x, double y) {
+        Product result = null;
+        switch (productType) {
+            case EGG: result = new Egg(x, y);
+                break;
+            // TODO: 12/31/2018 complete this shit
+
+            default: break;
+        }
+
+        return result;
+    }
 }
