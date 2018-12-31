d23f757656cff12cd2aac7e3f7aa5119ed2ad607
Aliiiqbp <aliiiqbp@gmail.com>
2018-12-31 17:48:55 +0330

    newwws product

--- src/Model/Product/EggPowder.java
+++ src/Model/Product/EggPowder.java
@@ -1,4 +1,10 @@
 package src.Model.Product;
 
+import src.Controller.Static;
+
 public class EggPowder extends Product {
+
+    public EggPowder(double x, double y){
+        super(ProductType.EGGPOWDER, Static.EGGP);
+    }
 }
--- src/Model/Product/ProductType.java
+++ src/Model/Product/ProductType.java
@@ -1,16 +1,57 @@
 package src.Model.Product;
 
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+
 public enum ProductType {
-    EGG, FLOUR, GRASS, MILK, WOOL, WATER;
+    EGG, FLOUR, GRASS, MILK, WOOL, WATER, COOKIE, CAKE, EGGPOWDER, FABRIC, PLUME, CARNIVALDRESS, SEWING;
 
     public Product getProduct(ProductType productType, double x, double y) {
         Product result = null;
         switch (productType) {
-            case EGG: result = new Egg(x, y);
+            case EGG:
+                result = new Egg(x, y);
                 break;
+            case MILK:
+                result = new Milk(x, y);
+                break;
+            case FLOUR:
+                result = new Flour(x, y);
+                break;
+            case GRASS:
+                result = new Grass(x, y);
+                break;
+            case WOOL:
+                result = new Wool(x, y);
+                break;
+            case WATER:
+                result = new Water(x, y);
+                break;
+            case COOKIE:
+                result = new Cookie(x, y);
+                break;
+            case CAKE:
+                result = new Cake(x, y);
+                break;
+            case EGGPOWDER:
+                result = new EggPowder(x, y);
+                break;
+            case FABRIC:
+                result = new Fabric(x, y);
+                break;
+            case PLUME:
+                result = new Plume(x, y);
+                break;
+            case CARNIVALDRESS:
+                result = new CarnnivalDress(x, y);
+                break;
+            case SEWING:
+                result = new Sewing(x, y);
+                break;
+        }
             // TODO: 12/31/2018 complete this shit
-
-            default: break;
+            case default:
+                result = null;
+                break;
         }
 
         return result;
