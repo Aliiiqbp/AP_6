1e937904015f31b874f7c3da18b0c94dbfdf1d35
Aliiiqbp <aliiiqbp@gmail.com>
2018-12-31 23:01:55 +0330

    .

--- src/Model/Product/EggPowder.java
+++ src/Model/Product/EggPowder.java
@@ -5,6 +5,6 @@ import src.Controller.Static;
 public class EggPowder extends Product {
 
     public EggPowder(double x, double y){
-        super(ProductType.EGGPOWDER, Static.EGGP);
+        super(ProductType.EGGPOWDER, Static.);
     }
 }
