a2a3bfd3b3ee41fbc04ee2dbc0aeca7d47ac00b7
ali <aliiiqbp@gmail.com>
2018-12-31 16:51:34 +0330

    Map and Cell class created they have ArrayList

--- src/Model/Farm/Map/Cell.java
+++ src/Model/Farm/Map/Cell.java
@@ -6,9 +6,47 @@ import java.util.ArrayList;
 
 public class Cell {
 
+    private int x, y;
     private ArrayList<Entity> objects;
+    private Map map;
 
-    public Cell() {
+    public Cell(int x, int y, Map map) {
+        this.x = x;
+        this.y = y;
+        this.map = map;
         objects = new ArrayList<>();
+    }
+
+    public void removeFromCell(Entity object) {
+        if (objects.contains(object)) {
+            objects.remove(object);
+            this.map.removeObject(object, this);
+        }
+    }
+
+    public void addToCell(Entity object) {
+        if (!objects.contains(object)) {
+            objects.add(object);
+            this.map.addObject(object, this);
+        }
+    }
+
+    public boolean containObject(Entity object) {
+        if (objects.contains(object)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
--- src/Model/Farm/Map/Map.java
+++ src/Model/Farm/Map/Map.java
@@ -2,15 +2,54 @@ package src.Model.Farm.Map;
 
 import src.Model.Entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Map extends Entity {
 
     private Cell[][] cells;
+    private ArrayList<Entity> objects;
+    private HashMap<Entity, Cell> cellMap;
 
     public Map(int n) {
         cells = new Cell[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cells[i][j] = new Cell(i, j, this);
+            }
+        }
     }
 
     public Cell[][] getCells() {
         return cells;
+    }
+
+    public Cell getCell(int i, int j) {
+        return cells[i][j];
+    }
+
+    public void addObject(Entity entity, Cell cell) {
+        if (!objects.contains(entity)) {
+            objects.add(entity);
+            cellMap.put(entity, cell);
+        }
+    }
+
+    public void removeObject(Entity entity, Cell cell) {
+        if (objects.contains(entity)) {
+            objects.remove(entity);
+            cellMap.remove(entity, cell);
+        }
+    }
+
+    public boolean containObject(Entity entity) {
+        if (objects.contains(entity)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Cell getCellThatContain(Entity entity) {
+        return cellMap.get(entity);
     }
 }
