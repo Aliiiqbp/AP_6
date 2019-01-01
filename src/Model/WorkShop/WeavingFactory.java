7be461b9551e7b63593afb8cea00d9e0955cc337
ashkanlarni <ash.larni@gmail.com>
2019-01-01 17:28:05 +0330

    Weaving Factory's Constructor

--- src/Model/WorkShop/WeavingFactory.java
+++ src/Model/WorkShop/WeavingFactory.java
@@ -1,5 +1,11 @@
 package src.Model.WorkShop;
 
+import src.Model.Product.ProductType;
+
 public class WeavingFactory extends WorkShop{
+    public WeavingFactory() {
+        super(WorkShopType.WEAVING_FACTORY, ProductType.FABRIC);
+        this.getRequirements().put(ProductType.SEWING, 1);
+    }
 
 }
