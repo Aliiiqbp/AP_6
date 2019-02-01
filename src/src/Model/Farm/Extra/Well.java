99c84508487a8883f4cd19e3d891cd81055ada51
ashkanlarni <Aliiiqbp@gmail.com>
2019-02-02 00:23:14 +0330

    yo

---
+++ src/src/Model/Farm/Extra/Well.java
@@ -0,0 +1,137 @@
+package src.Model.Farm.Extra;
+
+import javafx.animation.Animation;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import src.Controller.Static;
+import src.GUI.AnimalSpriteAnimation;
+import src.Model.Building;
+import src.Model.Coordinate.Movement;
+import src.Model.Entity;
+import src.Model.Product.Water;
+
+import java.io.FileInputStream;
+
+public class Well extends Building {
+    private int wellCapacity;
+    private int amountOfWater;
+    private double fillWellCost;
+    private Movement movement;
+    protected ImageView lastImageView = null;
+
+    public Well() {
+        wellCapacity = Static.WELL_CAPACITY_LVL_0;
+        amountOfWater = wellCapacity;
+        fillWellCost = Static.WELL_FILL_COST_LVL_0;
+        movement = new Movement();
+    }
+
+    @Override
+    public void upgradeLevel() {
+        switch (getLevel()) {
+            case 0:
+                this.setFillWellCost(Static.WELL_FILL_COST_LVL_1);
+                this.setWellCapacity(Static.WELL_CAPACITY_LVL_1);
+                break;
+            case 1:
+                this.setFillWellCost(Static.WELL_FILL_COST_LVL_2);
+                this.setWellCapacity(Static.WELL_CAPACITY_LVL_2);
+                break;
+            case 2:
+                this.setFillWellCost(Static.WELL_FILL_COST_LVL_3);
+                this.setWellCapacity(Static.WELL_CAPACITY_LVL_3);
+                break;
+            case 3: //it's not upgradable :)
+                break;
+        }
+        increaseLevel();
+    }
+
+    public double getAmountOfWater() {
+        return amountOfWater;
+    }
+
+    public double getWellCapacity() {
+        return wellCapacity;
+    }
+
+    public double wellChargeCost() {
+        return fillWellCost;
+    }
+
+    public void chargeWell() {
+        // TODO: 2/1/2019 handle time
+        if (getFarm().getBank().canDecrease(fillWellCost)) {
+            getFarm().getBank().buy(fillWellCost);
+            this.amountOfWater = this.wellCapacity;
+        }
+
+    }
+
+    public boolean canGetWater() {
+        if (amountOfWater > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public Water getWater() {
+        if (canGetWater()) {
+            amountOfWater -= 1;
+            return new Water(this.movement.getCurrentX(), this.movement.getCurrentY());
+        }
+        return null;
+    }
+
+    private void setFillWellCost(double fillWellCost) {
+        this.fillWellCost = fillWellCost;
+    }
+
+    private void setWellCapacity(int wellCapacity) {
+        this.wellCapacity = wellCapacity;
+    }
+
+    public void showWell(Group root){
+        try {
+            Image wellImage = new Image(new FileInputStream("src/Resources/Textures/Service/Well/" + Integer.toString(0)  + Integer.toString(this.getLevel()) +".png"));
+            ImageView wellView = new ImageView(wellImage);
+            wellView.setX(580);
+            wellView.setY(40);
+            root.getChildren().remove(lastImageView);
+            root.getChildren().add(wellView);
+
+            final int count = 16;
+            final int durationTime = 2000, offsetX = 0, offsetY = 0;
+            int columns = 4, width = (int) wellImage.getWidth(), height = (int) wellImage.getHeight();
+
+
+            wellView.setViewport(new Rectangle2D(0, 0, width, height));    //sprite animation  useful for your project
+            final Animation animation = new AnimalSpriteAnimation(
+                    wellView,
+                    Duration.millis(durationTime),
+                    count, columns,
+                    offsetX, offsetY,
+                    (int) Math.ceil(1.0 * width / columns), (int) Math.ceil(1.0 * height / (1.0 * count / columns))
+            );
+            animation.setCycleCount(Animation.INDEFINITE);
+            animation.play();
+
+            wellView.setOnMouseClicked(event -> {
+                ///// it fills the well
+                this.chargeWell();
+
+            });
+
+            lastImageView = wellView;
+        } catch (Exception e) {
+        }
+
+    }
+
+    public double getFillWellCost() {
+        return fillWellCost;
+    }
+}
